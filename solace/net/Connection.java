c50307daf50c8f4acb4fa8bd8037a9612c1b35df
Ryan Sandor Richards <sandor.richards@gmail.com>
2015-04-11 23:23:48 -0700

    Fixed whitespace for net/

--- solace/net/Connection.java
+++ solace/net/Connection.java
@@ -8,217 +8,220 @@ import solace.game.*;
 import solace.cmd.*;
 
 /**
- * Player connection object, handles basic user input and output, login and main menu.
+ * Player connection object, handles basic user input and output, login and main
+ * menu.
  * @author Ryan Sandor Richards (Gaius)
  */
 public class Connection
-	implements Runnable
+  implements Runnable
 {
-	Socket socket;
-	PrintWriter out;
-	BufferedReader in;
-	String prompt = "";
-	Account account;
-	boolean color = false;
-	StateController controller;
-	Date connectionTime;
-	
-	// Useful for disabling characters while major game actions are taking place
-	// such as area reloading or reboots. See the setIgnoreInput() method.
-	boolean ignoreInput = false;
-	
-	/**
-	 * Creates a new connection through the given socket.
-	 * @param s Socket for the connection
-	 * @throws IOException If the input and output streams could not be used for the socket.
-	 */
-	public Connection(Socket s)
-		throws IOException
-	{
-		socket = s;
-		connectionTime = new Date();
-		out = new PrintWriter(socket.getOutputStream());
-		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		controller = new LoginController(this);
-	}
+  Socket socket;
+  PrintWriter out;
+  BufferedReader in;
+  String prompt = "";
+  Account account;
+  boolean color = false;
+  StateController controller;
+  Date connectionTime;
 
-	/**
-	 * Sets the state controller for the connection.
-	 * @param c State controller.
-	 */
-	public void setStateController(StateController c)
-	{
-		controller = c;
-	}
-	
-	/**
-	 * @return The user's state controller.
-	 */
-	public StateController getStateController() {
-		return controller;
-	}
-	
-	/**
-	 * Closes this connection.
-	 */
-	public void close()
-	{
-		try {
-			socket.close();
-		}
-		catch (IOException ioe) 
-		{
-			Log.error(ioe.getMessage());
-		}
-	}
-	
-	/**
-	 * Turns client echo off.
-	 */
-	public void echoOff() {
-		try {
-			int[] cmd = {255, 251, 1};
-			for (int i : cmd) 
-				socket.getOutputStream().write(i);
-			byte[] bytes = {0, 0, 0};
-			socket.getInputStream().read(bytes);
-		}
-		catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Turns client echo on.
-	 */
-	public void echoOn() {
-		try {
-			int[] cmd = {255, 252, 1};
-			for (int i : cmd) 
-				socket.getOutputStream().write(i);
-			byte[] bytes = {0, 0, 0};
-			socket.getInputStream().read(bytes);
-		}
-		catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-	}
+  // Useful for disabling characters while major game actions are taking place
+  // such as area reloading or reboots. See the setIgnoreInput() method.
+  boolean ignoreInput = false;
 
-	
-	/**
-	 * Basic output function for connections.
-	 * @param s String to send.
-	 */
-	public void send(String s)
-	{
-		// Handle color formatting
-		if (color)
-			s = Color.format(s);
-		else
-			s = Color.strip(s);
-		
-		// Output and flush information through the socket
-		out.print(s);
-		out.flush();
-	}
-	
-	/**
-	 * Sends a string followed by a newline to the connection
-	 * @param s String to send.
-	 */
-	public void sendln(String s)
-	{
-		send(s+"\n\r");
-	}
-		
-	/**
-	 * Continuously collects commands from the user and handles them.
-	 */
-	public void run()
-	{
-		setPrompt("Use ANSI Color [Y/N]? ");
-		try 
-		{
-			while (socket.isConnected())
-			{
-				send(prompt);
-				String input = in.readLine();
-				if (input != null && !ignoreInput)
-					controller.parse(input);
-			}
-		}
-		catch (IOException ioe)
-		{
-			close();
-		}
-	}
+  /**
+   * Creates a new connection through the given socket.
+   * @param s Socket for the connection
+   * @throws IOException If the input and output streams could not be used for
+   *   the socket.
+   */
+  public Connection(Socket s)
+    throws IOException
+  {
+    socket = s;
+    connectionTime = new Date();
+    out = new PrintWriter(socket.getOutputStream());
+    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    controller = new LoginController(this);
+  }
 
-	/**
-	 * @return the prompt
-	 */
-	public String getPrompt() {
-		return prompt;
-	}
+  /**
+   * Sets the state controller for the connection.
+   * @param c State controller.
+   */
+  public void setStateController(StateController c)
+  {
+    controller = c;
+  }
 
-	/**
-	 * @param prompt the prompt to set
-	 */
-	public void setPrompt(String prompt) {
-		this.prompt = prompt;
-	}
+  /**
+   * @return The user's state controller.
+   */
+  public StateController getStateController() {
+    return controller;
+  }
 
-	/**
-	 * Determines if the connection has a logged in account. 
-	 * @return True if the connection has an account, false otherwise.
-	 */
-	public boolean hasAccount()
-	{
-		return (account != null);
-	}
-	
-	/**
-	 * @return the account
-	 */
-	public Account getAccount() {
-		return account;
-	}
+  /**
+   * Closes this connection.
+   */
+  public void close()
+  {
+    try {
+      socket.close();
+    }
+    catch (IOException ioe)
+    {
+      Log.error(ioe.getMessage());
+    }
+  }
 
-	/**
-	 * @param account the account to set
-	 */
-	public void setAccount(Account account) {
-		this.account = account;
-	}
-	
-	/**
-	 * Returns the internet address from which this connection is connected.
-	 * @return The internet address of the connection.
-	 */
-	public InetAddress getInetAddress()
-	{
-		return socket.getInetAddress();
-	}
-	
-	/**
-	 * Sets whether or not this connection uses ANSI color.
-	 * @param c True if the connection is to use color, false otherwise.
-	 */
-	public void setUseColor(boolean c)
-	{
-		color = c;
-	}
-	
-	/**
-	 * @return the connectionTime
-	 */
-	public Date getConnectionTime() {
-		return connectionTime;
-	}
-	
-	/**
-	 * @param b Whether or not the current connection should ignore input from the user.
-	 */
-	public void setIgnoreInput(boolean b) {
-		ignoreInput = b;
-	}
+  /**
+   * Turns client echo off.
+   */
+  public void echoOff() {
+    try {
+      int[] cmd = {255, 251, 1};
+      for (int i : cmd)
+        socket.getOutputStream().write(i);
+      byte[] bytes = {0, 0, 0};
+      socket.getInputStream().read(bytes);
+    }
+    catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+
+  /**
+   * Turns client echo on.
+   */
+  public void echoOn() {
+    try {
+      int[] cmd = {255, 252, 1};
+      for (int i : cmd)
+        socket.getOutputStream().write(i);
+      byte[] bytes = {0, 0, 0};
+      socket.getInputStream().read(bytes);
+    }
+    catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+
+
+  /**
+   * Basic output function for connections.
+   * @param s String to send.
+   */
+  public void send(String s)
+  {
+    // Handle color formatting
+    if (color)
+      s = Color.format(s);
+    else
+      s = Color.strip(s);
+
+    // Output and flush information through the socket
+    out.print(s);
+    out.flush();
+  }
+
+  /**
+   * Sends a string followed by a newline to the connection
+   * @param s String to send.
+   */
+  public void sendln(String s)
+  {
+    send(s+"\n\r");
+  }
+
+  /**
+   * Continuously collects commands from the user and handles them.
+   */
+  public void run()
+  {
+    setPrompt("Use ANSI Color [Y/N]? ");
+    try
+    {
+      while (socket.isConnected())
+      {
+        send(prompt);
+        String input = in.readLine();
+        if (input != null && !ignoreInput)
+          controller.parse(input);
+      }
+    }
+    catch (IOException ioe)
+    {
+      close();
+    }
+  }
+
+  /**
+   * @return the prompt
+   */
+  public String getPrompt() {
+    return prompt;
+  }
+
+  /**
+   * @param prompt the prompt to set
+   */
+  public void setPrompt(String prompt) {
+    this.prompt = prompt;
+  }
+
+  /**
+   * Determines if the connection has a logged in account.
+   * @return True if the connection has an account, false otherwise.
+   */
+  public boolean hasAccount()
+  {
+    return (account != null);
+  }
+
+  /**
+   * @return the account
+   */
+  public Account getAccount() {
+    return account;
+  }
+
+  /**
+   * @param account the account to set
+   */
+  public void setAccount(Account account) {
+    this.account = account;
+  }
+
+  /**
+   * Returns the internet address from which this connection is connected.
+   * @return The internet address of the connection.
+   */
+  public InetAddress getInetAddress()
+  {
+    return socket.getInetAddress();
+  }
+
+  /**
+   * Sets whether or not this connection uses ANSI color.
+   * @param c True if the connection is to use color, false otherwise.
+   */
+  public void setUseColor(boolean c)
+  {
+    color = c;
+  }
+
+  /**
+   * @return the connectionTime
+   */
+  public Date getConnectionTime() {
+    return connectionTime;
+  }
+
+  /**
+   * @param b Whether or not the current connection should ignore input from the
+   *   user.
+   */
+  public void setIgnoreInput(boolean b) {
+    ignoreInput = b;
+  }
 }
--- solace/net/Server.java
+++ solace/net/Server.java
@@ -11,117 +11,122 @@ import java.util.*;
  * Main game server, listens for and manages connections to the game.
  * @author Ryan Sandor Richards (Gaius)
  */
-public class Server 
+public class Server
 {
-	private int port;
-	private ServerSocket serverSocket;
-	private boolean listening = false;
-	LinkedList connections = new LinkedList();
-	
-	/**
-	 * Creates a new server for listening on port 4000.
-	 * @throws IOException If the server was unable to be initialized on port 4000.
-	 */
-	public Server()
-		throws IOException
-	{
-		initialize(4000);
-	}
-	
-	/**
-	 * Creates a new server for listening on the given port.
-	 * @param port Port for the server to listen.
-	 * @throws IOException If the server could not be initialized on the given port.
-	 */
-	public Server(int port)
-		throws IOException
-	{
-		initialize(port);
-	}
-	
-	/**
-	 * Initalizes the game server.
-	 * @param p Port to listen on.
-	 * @throws IOException If the server could not be initialized properly on the given port.
-	 */
-	protected void initialize(int p)
-		throws IOException
-	{
-		port = p;
-		serverSocket = new ServerSocket(port);
-		Log.info("Game Server Initialized");
-	}
-	
-	/**
-	 * Begins the main listen loop for the server.
-	 */
-	public void listen()
-	{
-		listening = true;
-		Log.info("Game server running and listening on port " + port);
-		
-		while (listening)
-		{
-			try 
-			{
-				Socket s = serverSocket.accept();
-				Log.info("Incoming connection from " + s.getInetAddress().toString());
-				initSocket(s);
-			}
-			catch (IOException ioe)
-			{
-				if (listening)
-					Log.error("Unable to accept connection on port " + port);
-			}
-		}
-	}
-	
-	/**
-	 * Initializes an incoming socket and connects the player to the game.
-	 * @param s Socket to initialize.
-	 */
-	public void initSocket(Socket s)
-		throws IOException
-	{
-		// Create the new connection
-		Connection c = new Connection(s);
-		
-		// Add the connection to the game world
-		World.addConnection(c);
-		
-		// Start the connection thread
-		(new Thread(c)).start();
-	}
-	
-	/**
-	 * Shuts the server down. This entails dropping all current connections
-	 * and ending the listening loop.
-	 */
-	public void shutdown()
-	{
-		listening = false;
-		
-		/*
-		 * Close all connections to the server.
-		 */
-		Collection connections = Collections.synchronizedCollection(World.getConnections());
-		synchronized (connections)
-		{
-			Iterator i = connections.iterator();
-			while (i.hasNext())
-			{
-				Connection c = (Connection)i.next();
-				c.sendln("\n{RServer Shutdown!{x\n");
-				c.close();
-			}
-		}
-		
-		try {
-			serverSocket.close();
-		}
-		catch (IOException ioe)
-		{
-			Log.error("I/O Error on server socket close during shutdown");
-		}
-	}
+  private int port;
+  private ServerSocket serverSocket;
+  private boolean listening = false;
+  LinkedList connections = new LinkedList();
+
+  /**
+   * Creates a new server for listening on port 4000.
+   * @throws IOException If the server was unable to be initialized on port
+   *   4000.
+   */
+  public Server()
+    throws IOException
+  {
+    initialize(4000);
+  }
+
+  /**
+   * Creates a new server for listening on the given port.
+   * @param port Port for the server to listen.
+   * @throws IOException If the server could not be initialized on the given
+   *   port.
+   */
+  public Server(int port)
+    throws IOException
+  {
+    initialize(port);
+  }
+
+  /**
+   * Initalizes the game server.
+   * @param p Port to listen on.
+   * @throws IOException If the server could not be initialized properly on the
+   *   given port.
+   */
+  protected void initialize(int p)
+    throws IOException
+  {
+    port = p;
+    serverSocket = new ServerSocket(port);
+    Log.info("Game Server Initialized");
+  }
+
+  /**
+   * Begins the main listen loop for the server.
+   */
+  public void listen()
+  {
+    listening = true;
+    Log.info("Game server running and listening on port " + port);
+
+    while (listening)
+    {
+      try
+      {
+        Socket s = serverSocket.accept();
+        Log.info("Incoming connection from " + s.getInetAddress().toString());
+        initSocket(s);
+      }
+      catch (IOException ioe)
+      {
+        if (listening)
+          Log.error("Unable to accept connection on port " + port);
+      }
+    }
+  }
+
+  /**
+   * Initializes an incoming socket and connects the player to the game.
+   * @param s Socket to initialize.
+   */
+  public void initSocket(Socket s)
+    throws IOException
+  {
+    // Create the new connection
+    Connection c = new Connection(s);
+
+    // Add the connection to the game world
+    World.addConnection(c);
+
+    // Start the connection thread
+    (new Thread(c)).start();
+  }
+
+  /**
+   * Shuts the server down. This entails dropping all current connections
+   * and ending the listening loop.
+   */
+  public void shutdown()
+  {
+    listening = false;
+
+    /*
+     * Close all connections to the server.
+     */
+    Collection connections = Collections.synchronizedCollection(
+      World.getConnections()
+    );
+    synchronized (connections)
+    {
+      Iterator i = connections.iterator();
+      while (i.hasNext())
+      {
+        Connection c = (Connection)i.next();
+        c.sendln("\n{RServer Shutdown!{x\n");
+        c.close();
+      }
+    }
+
+    try {
+      serverSocket.close();
+    }
+    catch (IOException ioe)
+    {
+      Log.error("I/O Error on server socket close during shutdown");
+    }
+  }
 }
