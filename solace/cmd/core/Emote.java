45ac8e618b8401d02bb690e3becfe36b54d58af8
Ryan Sandor Richards <sandor.richards@gmail.com>
2017-07-08 09:36:48 -0700

    Reworked the way emotes were loaded and used.

--- solace/cmd/core/Emote.java
+++ solace/cmd/core/Emote.java
@@ -11,13 +11,8 @@ import java.util.*;
  * @author Ryan Sandor Richards
  */
 public class Emote extends AbstractCommand {
-  HelpSystem help;
-  Emotes emotes;
-
   public Emote() {
-    super("emote");
-    emotes = Emotes.getInstance();
-    setAliases(emotes.getEmoteAliases());
+    super("emote", Emotes.getInstance().getEmoteAliases());
     setPriority(AbstractCommand.ORDER_LOW);
   }
 
@@ -35,9 +30,10 @@ public class Emote extends AbstractCommand {
         return;
       }
 
-      StringBuffer buffer = new StringBuffer();
+      StringBuilder buffer = new StringBuilder();
       for (int i = 1; i < params.length; i++) {
-        buffer.append(" " + params[i]);
+        buffer.append(" ");
+        buffer.append(params[i]);
       }
 
       String msg = buffer.toString().trim();
@@ -52,6 +48,7 @@ public class Emote extends AbstractCommand {
     }
 
     try {
+      Emotes emotes = Emotes.getInstance();
       if (params.length == 1) {
         player.wrapln(emotes.toSource(params[0]));
         room.sendMessage(emotes.toRoom(params[0]), player);
@@ -69,30 +66,24 @@ public class Emote extends AbstractCommand {
 
       player.resetVisibilityOnAction("emote");
 
-      // TODO This might need to use `room.sendMessage` instead...
+      // TODO This should be synchronized by the room itself :(
       Collection<Player> roomChars = room.getPlayers();
-      synchronized(roomChars) {
-        for (Player ch : roomChars) {
-          if (ch == player || ch == target) {
-            continue;
-          }
-          ch.sendMessage(emotes.toRoom(
-            emote,
-            player.getName(),
-            target.getName()
-          ));
+      for (Player ch : roomChars) {
+        if (ch == player || ch == target) {
+          continue;
         }
+        ch.sendMessage(emotes.toRoom(
+          emote,
+          player.getName(),
+          target.getName()
+        ));
       }
 
       player.sendln(emotes.toSource(emote, target.getName()));
       target.sendMessage(emotes.toTarget(emote, player.getName()));
     }
-    catch (EmoteNotFoundException enfe) {
+    catch (EmoteNotFoundException | InvalidEmoteException enfe) {
       Log.error(enfe.getMessage());
-      player.sendln("Could not emote. Try again later.");
-    }
-    catch (InvalidEmoteException iee) {
-      Log.error(iee.getMessage());
       player.sendln("Could not emote. Try again later.");
     }
   }
