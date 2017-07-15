c67ebcb77c8ceecbfb5805af195e5997ed1733e6
Ryan Sandor Richards <sandor.richards@gmail.com>
2017-07-14 22:38:34 -0700

    Fixed script imports due to refactor of io package.

--- scripts/common.js
+++ scripts/common.js
@@ -9,8 +9,8 @@
 this.Log = Packages.solace.util.Log;
 this.Strings = Packages.solace.util.Strings;
 this.Color = Packages.solace.util.Color;
-this.Config = Packages.solace.util.Config;
-this.Messages = Packages.solace.util.Messages;
+this.Config = Packages.solace.io.Config;
+this.Messages = Packages.solace.io.Messages;
 this.Roll = Packages.solace.util.Roll;
 
 this.format = java.lang.String.format;
