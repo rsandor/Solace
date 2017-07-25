035807cf881d73a46573d907f52372934d73300f
Ryan Sandor Richards <sandor.richards@gmail.com>
2017-07-24 23:28:29 -0700

    Updated dream manager to randomly give dreams.
    Blanks out random words to create a madlib style dream that is different every time.

--- solace/game/DreamManager.java
+++ solace/game/DreamManager.java
@@ -1,8 +1,15 @@
 package solace.game;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import solace.io.Config;
+import solace.io.Dreams;
 import solace.util.Clock;
 import solace.util.Log;
+import solace.util.Roll;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * Manages dreams for all sleeping characters in the game world.
@@ -38,8 +45,17 @@ public class DreamManager {
    * Performs a dream cycle for all sleeping players.
    */
   private static void cycle() {
-    Game.getActiveCharacters().stream().filter((c) -> c.isSleeping()).forEach((c) -> {
-      c.sendMessage("You continue to sleep blissfully.");
+    Game.getActiveCharacters().stream().filter(AbstractPlayer::isSleeping).forEach((c) -> {
+      // Determine if we present the player with a dream
+      if (Roll.uniform() <= 0.25) {
+        // Fetch a random dream and blank out random words
+        Stream<String> words = Arrays.stream(Dreams.getInstance().getRandom().split("\\s"));
+        c.wrapln(Joiner.on(" ").join(
+          words.map(word -> Roll.uniform() < 0.167 ? word.replaceAll(".", "_") : word).toArray()));
+        c.sendMessage("");
+      } else {
+        c.sendMessage("You are soundly asleep.");
+      }
     });
   }
 }
